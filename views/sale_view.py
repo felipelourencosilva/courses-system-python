803e0bccfa95346c5b1197c0cb97e22f17ecb972
Gabriel Guimarães <gabrielcastroguimaraes1@gmail.com>
2024-05-04 18:33:07 -0300

    fix: isnumeric function wasn't being called in several view classes.

--- views/sale_view.py
+++ views/sale_view.py
@@ -7,7 +7,7 @@ class SaleView:
 
         while True:
             chosen = input("Sua escolha: ")
-            if not chosen.isnumeric or int(chosen) not in [0]:
+            if not chosen.isnumeric() or int(chosen) not in [0]:
                 print("Por favor, escolha um número dentre as opções.")
                 continue
             return int(chosen)
