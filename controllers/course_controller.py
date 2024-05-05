b6a700e5e8f92d215694f28eda1f7abcb908db94
Felipe Lourenco <lipelourencosilva@gmail.com>
2024-05-04 23:01:09 -0300

    feat: add course_controller and course_view bodies

--- controllers/course_controller.py
+++ controllers/course_controller.py
@@ -1,3 +1,76 @@
+from views.course_view import *
+from entities.course import *
+import random
+
+
 class CourseController:
-    def __init__(self):
+    def __init__(self, system_controller):
+        self.__courses = dict()
+        self.__course_view = CourseView()
+        self.__system_controller = system_controller
+
+    def get_producer(self, cpf: int):
+        return self.__system_controller.producer_controller.get_producer_by_cpf(cpf)
+
+    def generate_id(self):
+        id = random.randint(1, 1000)
+        while id in self.__courses:
+            id = random.randint(1, 1000)
+        return id
+
+    def add_course(self):
+        course_data = self.__course_view.get_add_course_data()
+        id = self.generate_id()
+        course = Course(course_data["name"], self.get_producer(course_data["cpf"]),
+                        course_data["description"], course_data["price"], course_data["commission_percentage"], id)
+        self.__courses[id] = course
+
+    def edit_course(self):
+        self.list_courses()
+        id = self.__course_view.read_id()
+
+        if id is not None and id in self.__courses:
+            course_data = self.__course_view.get_edit_course_data()
+            course = self.__courses[id]
+            course.name = course_data["name"]
+            course.description = course_data["description"]
+            course.price = course_data["price"]
+            course.commission_percentage = course_data["commission_percentage"]
+        else:
+            self.__course_view.show_message("Curso não encontrado ou id incorreto")
+
+    def list_courses(self):
+        if len(self.__courses) == 0:
+            self.__course_view.show_message("Não há cursos cadastrados")
+        else:
+            for key, course in self.__courses:
+                self.__course_view.show_course({"name": course.name, "descriptin": course.email, "price": course.price,
+                                                "id": course.id})
+
+    def remove_course(self):
+        self.list_courses()
+        id = self.__course_view.read_id()
+
+        if id is not None and id in self.__courses:
+            self.__courses.pop(id)
+        else:
+            self.__course_view.show_message("Curso não encontrado ou id incorreto")
+
+    def buy_course(self):
         pass
+
+    def previous_view(self):
+        self.__system_controller.show_view()
+
+    def show_view(self):
+        options = {
+            1: self.add_course,
+            2: self.edit_course,
+            3: self.list_courses,
+            4: self.buy_course,
+            0: self.previous_view
+        }
+
+        while True:
+            chosen_option = self.__course_view.view_options()
+            options[chosen_option]()
--- views/course_view.py
+++ views/course_view.py
@@ -1,13 +1,56 @@
 class CourseView:
 
-    def view_options(self) -> int:
-        print("------- CURSO -------")
-        print("Escolha alguma opção:")
-        print("0 - Voltar")
+    def view_options(self):
+        print("-------- CURSOS ----------")
+        print("Escolha a opcao")
+        print("1 - Adicionar Curso")
+        print("2 - Editar Curso")
+        print("3 - Listar Cursos")
+        print("4 - Comprar Curso")
+        print("0 - Retornar")
 
         while True:
             option = input("Sua escolha: ")
-            if not option.isnumeric() or int(option) not in range(0, 1):
+            if not option.isnumeric() or int(option) not in range(0, 5):
                 print("Por favor, escolha um número dentre as opções.")
                 continue
             return int(option)
+
+    def get_add_course_data(self):
+        print("-------- DADOS CURSO ----------")
+        name = input("Nome: ")
+        description = input("Descrição: ")
+        price = float(input("Preço: "))
+        commission_percentage = int(input("Porcentagem da comissão (Ex: 15, 25, 50): "))
+        cpf = int(input("CPF do produtor: "))
+
+        return {"name": name, "description": description, "price": price,
+                "commission_percentage": commission_percentage, "cpf": cpf}
+
+    def get_edit_course_data(self):
+        print("-------- DADOS CURSO ----------")
+        name = input("Nome: ")
+        description = input("Descrição: ")
+        price = float(input("Preço: "))
+        commission_percentage = int(input("Porcentagem da comissão (Ex: 15, 25, 50): "))
+
+        return {"name": name, "description": description, "price": price,
+                "commission_percentage": commission_percentage}
+
+    def show_course(self, course_data):
+        print("Nome do curso: ", course_data["name"])
+        print("Descrição do curso: ", course_data["description"])
+        print("Preço: ", course_data["price"])
+        print("Id: ", course_data["id"])
+        print()
+
+    def read_id(self):
+        while True:
+            id = input("Digite o ID do curso: ")
+            if not id.isnumeric() or int(id) <= 0 or int(id) > 1000:
+                print("O ID precisa ser um inteiro maior que 0")
+                continue
+            return int(id)
+
+    def show_message(self, msg):
+        print(msg)
